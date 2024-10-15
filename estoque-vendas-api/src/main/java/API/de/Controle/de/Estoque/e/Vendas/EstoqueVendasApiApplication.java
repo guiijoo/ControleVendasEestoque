e155de3e32fd55db1899534d2e093c458963a946
guiijoo <gui.marchetto@hotmail.com>
2024-10-14 21:09:33 -0300

    adição initializer

---
+++ estoque-vendas-api/src/main/java/API/de/Controle/de/Estoque/e/Vendas/EstoqueVendasApiApplication.java
@@ -0,0 +1,13 @@
+package API.de.Controle.de.Estoque.e.Vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueVendasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueVendasApiApplication.class, args);
+	}
+
+}
---
+++ estoque-vendas-api/src/test/java/API/de/Controle/de/Estoque/e/Vendas/EstoqueVendasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package API.de.Controle.de.Estoque.e.Vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueVendasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
